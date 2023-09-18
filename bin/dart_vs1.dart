2ddea3908ca5bdae6cd2269ceff91360d3745a1e
jibinbala <jibinep36@gmail.com>
2023-09-18 12:50:32 +0530

    first commit

---
+++ bin/dart_vs1.dart
@@ -0,0 +1,5 @@
+import 'package:dart_vs1/dart_vs1.dart' as dart_vs1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_vs1.calculate()}!');
+}
---
+++ test/dart_vs1_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_vs1/dart_vs1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
